9c9282326a049cbf24790f5704d13da49bb1af6e
Anderlei Nascimento <89626821+AnderleiNascimento@users.noreply.github.com>
2025-05-06 16:27:40 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { BootstrapVueNextResolver } from 'bootstrap-vue-next';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/crm/",
+  base: "/",
   plugins: [
     vue(),
     Components({
